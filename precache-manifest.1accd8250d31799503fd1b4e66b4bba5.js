5ea71e2598b5e68736e56b9e0f30e31fdc9cb8e7
Timothy McHugh <Timothy.McHugh@yarde.com>
2019-12-11 14:36:39 -0500

    Updates

---
+++ precache-manifest.1accd8250d31799503fd1b4e66b4bba5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfb04f51b358bcf523027a2b87f0ee0",
+    "url": "/okta_yarde_staff_portal/index.html"
+  },
+  {
+    "revision": "5682131b4dc70e3f90fd",
+    "url": "/okta_yarde_staff_portal/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f6a590f4b41be02eb712",
+    "url": "/okta_yarde_staff_portal/static/js/2.8517d0b1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/okta_yarde_staff_portal/static/js/2.8517d0b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5682131b4dc70e3f90fd",
+    "url": "/okta_yarde_staff_portal/static/js/main.3b52bf33.chunk.js"
+  },
+  {
+    "revision": "a6726bca068765f1251b",
+    "url": "/okta_yarde_staff_portal/static/js/runtime-main.8395ca28.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/okta_yarde_staff_portal/static/media/logo.5d5d9eef.svg"
+  }
+]);
